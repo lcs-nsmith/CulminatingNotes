a92b30051ea4f3c66644522de7732116f96c4890
Nathan Smith <nsmith@lcs.on.ca>
2022-05-13 09:57:36 -0400

    Initial Commit

---
+++ CulminatingNotes/CulminatingNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CulminatingNotesApp.swift
+//  CulminatingNotes
+//
+//  Created by Nathan Smith on 2022-05-13.
+//
+
+import SwiftUI
+
+@main
+struct CulminatingNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
