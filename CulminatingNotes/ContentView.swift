b1598cdb09909fdc1c8ecd7e3bfc296827c501c6
Nathan Smith <nsmith@lcs.on.ca>
2022-05-13 12:34:10 -0400

    Notes for the culminating assignment 👍🏽

--- CulminatingNotes/ContentView.swift
+++ CulminatingNotes/ContentView.swift
@@ -9,7 +9,16 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        TextEditor(text: .constant("""
+    1. Input to the text field
+                            ⬇️
+    2. function pulls data from endpoint using search query from text field to populate search list
+
+    3. Hotspot adds item to favourites (with animation) item, shows up in the favourites list
+                            ⬇️
+    4. hotspot to clear favourites
+"""))
+            .frame(minHeight: 300)
             .padding()
     }
 }
